beedd826e262621540d1ce2f57d1adf29fb9981d
hyee0715 <hyee0715@naver.com>
2022-11-23 17:02:14 +0900

    Initial commit

---
+++ src/main/java/com/hy/general_board_project/GeneralBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hy.general_board_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralBoardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hy/general_board_project/GeneralBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.general_board_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
