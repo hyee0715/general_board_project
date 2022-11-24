0a4d8ddddf19ce6a12d0e8b295ad9a5cf68f151f
hyee0715 <hyee0715@naver.com>
2022-11-24 16:04:31 +0900

    feat: 게시물 작성 view 구현

---
+++ src/main/java/com/hy/general_board_project/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.hy.general_board_project.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+@RequestMapping("board")
+public class BoardController {
+
+    @GetMapping("/write")
+    public String write() {
+        return "board/write";
+    }
+}
