21f206b210a363db3e9ada3efe6feafa14d19940
hyee0715 <hyee0715@naver.com>
2022-11-25 14:45:40 +0900

    feat: 게시판 목록 조회하는 기능 구현

--- src/main/java/com/hy/general_board_project/web/controller/IndexController.java
+++ src/main/java/com/hy/general_board_project/web/controller/IndexController.java
@@ -1,11 +1,29 @@
 package com.hy.general_board_project.web.controller;
 
+import com.hy.general_board_project.service.BoardService;
+import com.hy.general_board_project.web.dto.BoardListResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@Controller
 public class IndexController {
 
+    private BoardService boardService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<BoardListResponseDto> boardList = boardService.getBoardList();
+        model.addAttribute("boardList", boardList);
         return "index";
     }
+
+
 }
