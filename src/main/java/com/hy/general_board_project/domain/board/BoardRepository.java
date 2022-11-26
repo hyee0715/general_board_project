ef7a468ff439ba766cd8bc48e92d1f336f760438
hyee0715 <hyee0715@naver.com>
2022-11-26 21:06:16 +0900

    feat: 게시물 검색 기능 구현

--- src/main/java/com/hy/general_board_project/domain/board/BoardRepository.java
+++ src/main/java/com/hy/general_board_project/domain/board/BoardRepository.java
@@ -2,5 +2,8 @@ package com.hy.general_board_project.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitleContaining(String keyword);
 }
