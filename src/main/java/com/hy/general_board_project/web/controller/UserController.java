419e6043aa5d2e6506c8b543c69be22908962137
hyee0715 <hyee0715@naver.com>
2022-12-05 18:43:22 +0900

    refactor: Usercontroller 코드 리팩토링

--- src/main/java/com/hy/general_board_project/web/controller/UserController.java
+++ src/main/java/com/hy/general_board_project/web/controller/UserController.java
@@ -1,21 +1,15 @@
 package com.hy.general_board_project.web.controller;
 
 import com.hy.general_board_project.service.UserService;
-import com.hy.general_board_project.web.dto.user.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @AllArgsConstructor
 @Controller
 public class UserController {
-
-    private UserService userService;
 
     @GetMapping("/user/login")
     public String account() {
@@ -23,9 +17,7 @@ public class UserController {
     }
 
     @GetMapping("/user/signUp")
-    public String signUp(Model model) {
-        model.addAttribute("userDto", new UserDto());
-
+    public String signUp() {
         return "account/signUp";
     }
 }
