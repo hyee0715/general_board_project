b754bc8239227bfd181f1df407d422522feac465
hyee0715 <hyee0715@naver.com>
2022-12-14 10:12:49 +0900

    feat: 새 비밀 번호 유효성 검증 기능 구현

--- src/main/java/com/hy/general_board_project/web/controller/SettingController.java
+++ src/main/java/com/hy/general_board_project/web/controller/SettingController.java
@@ -1,5 +1,7 @@
 package com.hy.general_board_project.web.controller;
 
+import com.hy.general_board_project.domain.user.User;
+import com.hy.general_board_project.domain.user.UserRepository;
 import com.hy.general_board_project.service.BoardService;
 import com.hy.general_board_project.service.SettingService;
 import com.hy.general_board_project.validator.CheckNicknameValidator;
@@ -8,6 +10,8 @@ import com.hy.general_board_project.web.dto.user.UserInfoUpdateRequestDto;
 import com.hy.general_board_project.web.dto.user.UserPasswordUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +22,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -28,6 +33,7 @@ public class SettingController {
     private final SettingService settingService;
     private final CheckNicknameValidator checkNicknameValidator;
     private final BoardService boardService;
+    private final UserRepository userRepository;
 
     @InitBinder
     public void validatorBinder(WebDataBinder binder) {
@@ -92,7 +98,21 @@ public class SettingController {
     }
 
     @PostMapping("/setting/userPassword")
-    public String updateUserPassword(@ModelAttribute UserPasswordUpdateRequestDto userPasswordUpdateRequestDto) {
+    public String updateUserPassword(@Validated @ModelAttribute UserPasswordUpdateRequestDto userPasswordUpdateRequestDto, BindingResult bindingResult) {
+
+        if (!StringUtils.hasText(userPasswordUpdateRequestDto.getNewPassword())) {
+            bindingResult.rejectValue("password", "required", "");
+        } else {
+            if (!Pattern.matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", userPasswordUpdateRequestDto.getNewPassword())) {
+                bindingResult.addError(new FieldError("userPasswordUpdateRequestDto", "newPassword", userPasswordUpdateRequestDto.getNewPassword(), false, null, null, "비밀번호는 영문 대소문자, 숫자, 특수기호 1개 이상 포함 8자 ~ 20자 제한입니다."));
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "setting/userPassword";
+        }
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         String encodedNewPassword = encoder.encode(userPasswordUpdateRequestDto.getNewPassword());
