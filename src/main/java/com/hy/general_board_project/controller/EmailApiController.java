995848e099aaf71195f814589b7088a397235782
hyee0715 <hyee0715@naver.com>
2023-04-08 15:05:03 +0900

    fix: EmailApiController의 sendMail을 Post 형식으로 변경

--- src/main/java/com/hy/general_board_project/controller/EmailApiController.java
+++ src/main/java/com/hy/general_board_project/controller/EmailApiController.java
@@ -4,7 +4,7 @@ import com.hy.general_board_project.service.EmailService;
 import com.hy.general_board_project.domain.dto.user.UserSignUpRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class EmailApiController {
 
     private final EmailService emailService;
 
-    @GetMapping(value = "/user/email/send")
+    @PostMapping(value = "/user/email/send")
     public ResponseEntity sendMail(UserSignUpRequestDto userDto) throws MessagingException {
         String emailContent = emailService.makeEmailContentForJoin(userDto);
 
